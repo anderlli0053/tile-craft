72300baf9ae5292dc490386f22e4cc76edc68ce0
Coder2195Text <coder2195mail@gmail.com>
2022-03-04 16:34:46 -0500

    save

--- src/gameScene/game_scene.gd
+++ src/gameScene/game_scene.gd
@@ -1,17 +1,12 @@
 extends Node2D
 
 const Tile = preload("res://sprites/gameScene/Tile.tscn")
-const Resizer = preload("res://src/global/resize.gd")
 
 var tiles = {}
 func _ready():
 	tiles["stuff"] = Tile.instance()
 	add_child(tiles["stuff"])
 	$Camera.current = true
-
-func _on_resize():
-	Resizer._on_resize()
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- src/mainMenu/main_menu.gd
+++ src/mainMenu/main_menu.gd
@@ -1,11 +1,9 @@
 extends Control
-const Resizer = preload("res://src/global/resize.gd")
 
 func _on_ready():
 	$Camera.current = true
-	Resizer._on_start()
-	print("hi")
+
+
 
 func _on_resize():
-	Resizer._on_resize()
-	print(OS.get_window_safe_area().size)
+	print("d")
