b98398ac3e929ea088e5d8787cf7dc8d8da81ea0
Coder2195Text <coder2195mail@gmail.com>
2022-03-08 18:29:02 -0500

    commit

--- src/gameScene/player.gd
+++ src/gameScene/player.gd
@@ -10,14 +10,15 @@ func _ready():
 func _physics_process(delta):
 	if Input.is_action_pressed("left"):
 		velocity.x += -CONSTANTS.PLAYER_SPEED
-	elif Input.is_action_pressed("right"):
+	if Input.is_action_pressed("right"):
 		velocity.x += CONSTANTS.PLAYER_SPEED
 	if Input.is_action_just_pressed("up"):
 		print(is_on_floor())
 		if is_on_floor():
-			velocity.y = -100 * delta	
+			velocity.y = -100 * delta
+			print(velocity.y)
 	velocity.x *= 0.7
 	velocity.y += CONSTANTS.GRAVITY * delta
-	print(velocity.y)
+	
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
