b284fb5f4de79626eb95c53173f2b9b1ec183158
Coder2195Text <coder2195mail@gmail.com>
2022-03-08 18:25:37 -0500

    what the hell is happening

--- src/gameScene/player.gd
+++ src/gameScene/player.gd
@@ -12,10 +12,12 @@ func _physics_process(delta):
 		velocity.x += -CONSTANTS.PLAYER_SPEED
 	elif Input.is_action_pressed("right"):
 		velocity.x += CONSTANTS.PLAYER_SPEED
-	if Input.is_action_pressed("up"):
+	if Input.is_action_just_pressed("up"):
 		print(is_on_floor())
 		if is_on_floor():
-			velocity.y -= 30
+			velocity.y = -100 * delta	
 	velocity.x *= 0.7
 	velocity.y += CONSTANTS.GRAVITY * delta
-	velocity = move_and_slide(velocity, Vector2(0, -1))
+	print(velocity.y)
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
