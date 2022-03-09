6fa7350e2ddd3f3835f0cdb5da9303f6537775c7
Coder2195Text <coder2195mail@gmail.com>
2022-03-08 19:22:48 -0500

    yay yay its fixed

--- src/gameScene/player.gd
+++ src/gameScene/player.gd
@@ -1,24 +1,26 @@
 extends KinematicBody2D
 
-const CONSTANTS = preload("res://src/misc/constants.gd")
-
 var velocity = Vector2.ZERO
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+export var player_speed = 64
+export var jump_speed = 15000
 
 func _ready():
 	set_position(Vector2(0, -60))
 
 func _physics_process(delta):
 	if Input.is_action_pressed("left"):
-		velocity.x += -CONSTANTS.PLAYER_SPEED
+		velocity.x += -player_speed
 	if Input.is_action_pressed("right"):
-		velocity.x += CONSTANTS.PLAYER_SPEED
-	if Input.is_action_just_pressed("up"):
+		velocity.x += player_speed
+	if Input.is_action_pressed("up"):
 		print(is_on_floor())
 		if is_on_floor():
-			velocity.y = -100 * delta
+			velocity.y = -20000 * delta
 			print(velocity.y)
 	velocity.x *= 0.7
-	velocity.y += CONSTANTS.GRAVITY * delta
+	velocity.y += gravity * delta
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
--- src/misc/constants.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-const GRAVITY = 9.81 * 64
-const PLAYER_SPEED = 1 * 64
