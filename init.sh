0d6f374e79c15638cd61967cc1dde5bc77ed2019
Coder2195Text <coder2195mail@gmail.com>
2022-06-18 15:34:07 +0000

    try for gitpod

---
+++ init.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+
+wget https://downloads.tuxfamily.org/godotengine/3.4.4/mono/Godot_v3.4.4-stable_mono_x11_64.zip
+mv Godot_v3.4.4-stable_mono_x11_64.zip Godot.zip
+unzip Godot.zip
+mv Godot_v3.4.4-stable_mono_x11_64 Godot
+
