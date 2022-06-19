1e551c9599173e752d62960fb9a3bfce37aca6b0
Coder2195Text <coder2195mail@gmail.com>
2022-06-19 02:09:45 +0000

    clean up gp

--- init.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get update
-
-wget https://downloads.tuxfamily.org/godotengine/3.4.4/mono/Godot_v3.4.4-stable_mono_x11_64.zip
-mv Godot_v3.4.4-stable_mono_x11_64.zip Godot.zip
-unzip Godot.zip
-mv Godot_v3.4.4-stable_mono_x11_64 Godot
-rm Godot.zip
-mv Godot/Godot_v3.4.4-stable_mono_x11.64 Godot/Godot
